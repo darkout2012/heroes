2a8b5d0f92257f367e7e3eac0e80e59fc6f6379d
Bogdan Darkout <darkout2012@gmail.com>
2024-08-21 17:46:57 +0200

    feat: :sparkles: list component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent
+    HeroComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
