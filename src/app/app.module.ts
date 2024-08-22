94cad35f20eba6b66f13f9b10abbcb94043e4c7d
Bogdan Darkout <darkout2012@gmail.com>
2024-08-22 18:45:23 +0200

    feat: :sparkles: name and age added to hero.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
+import { HeroComponent } from './heroes/hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ListComponent,
     HeroComponent,
-    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
